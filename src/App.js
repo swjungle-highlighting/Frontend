9bb86c374e47bee177b2c86708446c4c6d5f49cf
Lenol <ek3434@naver.com>
2022-02-18 16:44:55 +0900

    package.json 설정 추가

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from "react";
 import { Route, Router, Switch, Link } from "react-router-dom";
-import Result from './pages/Result';
-import Loading from './pages/Loading';
-import Home from './pages/Home';
+import Result from "./pages/Result";
+import Loading from "./pages/Loading";
+import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
-import './App.css';
+import "./App.css";
 import AppStateProvider from "./providers/AppStateProvider";
 
 const App = () => {
   return (
     <>
       <AppStateProvider>
-          <Switch>
-            <Route path="/" exact={true} component={Home} />
-            <Route path="/loading" component={Loading} />
-            <Route path="/result" component={Result} />
-            <Route path="/notfound" component={NotFound} />
-            {/* <Route path="/result/:url" component={NotFound} /> */}
-          </Switch>
-          {/* <ul>
+        <Switch>
+          <Route path="/" exact={true} component={Home} />
+          <Route path="/loading" component={Loading} />
+          <Route path="/result" component={Result} />
+          <Route path="/notfound" component={NotFound} />
+          {/* <Route path="/result/:url" component={NotFound} /> */}
+        </Switch>
+        {/* <ul>
             <li>
               <Link to="/">Home</Link>
             </li>
